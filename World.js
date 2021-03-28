dd58e4a37ea91c7c6d355a68205b25d97381a9a2
444karat <sasha7971182@gmail.com>
2021-03-29 00:23:13 +0500

    3

--- World.js
+++ World.js
@@ -51,9 +51,6 @@ function init(){
     mrP = new Pack;
     mrG = new Enemy;
 
-    mrG.draw();
-    mrP.draw();
-
     for( var i = 0; i < map.length; i++ ){
         for(var j = 0; j < map[i].length; j++){
             if (map[i][j] == 1)  {
@@ -64,7 +61,9 @@ function init(){
                 ctx.drawImage(point,i*cellSize,j*cellSize,cellSize,cellSize);            
             }               
         }
-    }   
+    }
+    mrG.draw();
+    mrP.draw();   
 }
 
 function updateScreen(x,y){
@@ -85,16 +84,15 @@ Pack.prototype ={
         if(map[x][y] == 2){
             score++;
         }
-        if (map[x][y] == 3){
+        /*if (map[x][y] == 3){
             updateScreen(this.x,this.y);
             alert("dead");
             return;
-        }
+        }*/
         updateScreen(this.x,this.y);
         map[this.x][this.y] = 0;
         this.x = x;
         this.y = y;
-        map[this.x][this.y] = 4;
         this.draw();
     },
     isMove: function(x,y){
@@ -105,15 +103,15 @@ Pack.prototype ={
             return false;
         }
     }
-}
+};
 function Enemy(){
-    this.x = 1;
-    this.y = 1;
+    this.x = 10;
+    this.y = 10;
 }
 Enemy.prototype ={
     draw: function(){
-        map[this.x][this.y] = 3;
-        ctx.drawImage(ghost,this.y*cellSize,this.x*cellSize,cellSize,cellSize);
+        //map[this.x][this.y] = 3;
+        ctx.drawImage(ghost,this.x*cellSize,this.y*cellSize,cellSize,cellSize);
     },
     isMove: function(x,y){
         if(map[x][y] != 1){
@@ -123,14 +121,45 @@ Enemy.prototype ={
             return false;
         }
     },
-    move: function(){
-        
+    move: function(x,y){
+        //var step = map[x][y];
+        this.x = x;
+        this.y = y;
+        this.draw();
     },
     randGo: function(){
-        return Math.floor(Math.random() * (5 - 1)) + 1;
+        var where = Math.floor(Math.random() * (5 - 1)) + 1;
+        if (where == 1){//up
+            console.log("up");
+            if(this.isMove(this.x)[this.y-1]){
+                this.move(this.x,this.y-1);
+                
+            }
+        }
+        if (where == 2){//left
+            console.log("left");
+            if(this.isMove(this.x-1)[this.y]){
+                this.move(this.x-1,this.y);
+                
+            }
+        }
+        if (where == 3){//down
+            console.log("down");
+            if(this.isMove(this.x-1)[this.y]){
+                this.move(this.x-1,this.y);
+                
+            }
+        }
+        if (where == 4){//rigth
+            console.log("rigth");
+            if(this.isMove(this.x+1)[this.y]){
+                this.move(this.x+1,this.y);
+                
+            }
+        }
     }
 
-}
+};
 
 
 function onKeyDown(event){
@@ -173,6 +202,39 @@ window.onload = function(){
 
 
 ///canvas.addEventListener('keydown',onKeyDown,false);
+function randGo(){
+    var where = Math.floor(Math.random() * (5 - 1)) + 1;
+        if (where == 1){//up
+            console.log("up");
+            if(mrG.isMove(mrG.x)[mrG.y-1]){
+                mrG.move(mrG.x,mrG.y-1);
+                
+            }
+        }
+        if (where == 2){//left
+            console.log("left");
+            if(mrG.isMove(mrG.x-1)[mrG.y]){
+                mrG.move(mrG.x-1,mrG.y);
+                
+            }
+        }
+        if (where == 3){//down
+            console.log("down");
+            if(mrG.isMove(mrG.x-1)[mrG.y]){
+                mrG.move(mrG.x-1,mrG.y);
+                
+            }
+        }
+        if (where == 4){//rigth
+            console.log("rigth");
+            if(mrG.isMove(mrG.x+1)[mrG.y]){
+                mrG.move(mrG.x+1,mrG.y);
+                
+            }
+        }
+}
+
+
 
 document.onkeydown = function(event){
     var wCode = 87; 
@@ -186,28 +248,34 @@ document.onkeydown = function(event){
         start = false;
     }
     
+    
     switch(keyCode){
         case wCode:
             if (mrP.isMove(mrP.x, mrP.y-1)){             
                    mrP.move(mrP.x,mrP.y-1);
             }
+            randGo();
             break;
         case aCode:
             if(mrP.isMove(mrP.x-1,mrP.y)){
                 mrP.move(mrP.x-1,mrP.y);
             }
+            randGo();
+            
             break;    
         case sCode:
             if(mrP.isMove(mrP.x,mrP.y+1)){
                 mrP.move(mrP.x,mrP.y+1);
             }
+            //mrG.move(mrG.x,mrG.y+1);
+            randGo();
             break;
         case dCode:
             if(mrP.isMove(mrP.x+1,mrP.y)){
                 mrP.move(mrP.x+1,mrP.y);
             }
+            mrG.randGo();
             break;
     }
-
     console.log(score);
 }
