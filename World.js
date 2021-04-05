0a2e9448040729bfa24b92566d0f5a997def3a27
444karat <sasha7971182@gmail.com>
2021-04-05 22:30:50 +0500

    end

--- World.js
+++ World.js
@@ -6,6 +6,7 @@ var mrG;
 var FPS = 60;
 var cellSize = 35;
 var score = 0;
+var moving = false;
 
 
 
@@ -105,7 +106,7 @@ Pack.prototype ={
     }
 };
 function Enemy(){
-    this.x = 10;
+    this.x = 8;
     this.y = 10;
 }
 Enemy.prototype ={
@@ -115,119 +116,70 @@ Enemy.prototype ={
     },
     isMove: function(x,y){
         if(map[x][y] != 1){
+            moving = true;
             return true;
         }
         else{
+            moving = false;
             return false;
         }
     },
     move: function(x,y){
-        //var step = map[x][y];
+        moving = false;
+        var previously = map[this.x][this.y];
+
+        if(previously == 2){
+            updateScreen(this.x,this.y);
+            ctx.drawImage(point,this.x*cellSize,this.y*cellSize,cellSize,cellSize);            
+        }
+        if(previously == 0){
+            updateScreen(this.x,this.y);
+        }
+
+
         this.x = x;
         this.y = y;
         this.draw();
     },
     randGo: function(){
-        var where = Math.floor(Math.random() * (5 - 1)) + 1;
-        if (where == 1){//up
-            console.log("up");
-            if(this.isMove(this.x)[this.y-1]){
-                this.move(this.x,this.y-1);
-                
+        while(true){    
+            var where = Math.floor(Math.random() * (5 - 1)) + 1;
+            if (where == 1){//up
+                if(this.isMove(this.x,this.y-1)){
+                    this.move(this.x,this.y-1);
+                    console.log("up");
+                    break;
+                }
             }
-        }
-        if (where == 2){//left
-            console.log("left");
-            if(this.isMove(this.x-1)[this.y]){
-                this.move(this.x-1,this.y);
-                
+            if (where == 2){//left
+                if(this.isMove(this.x-1,this.y)){
+                    this.move(this.x-1,this.y);
+                    console.log("left");    
+                    break;
+                }
             }
-        }
-        if (where == 3){//down
-            console.log("down");
-            if(this.isMove(this.x-1)[this.y]){
-                this.move(this.x-1,this.y);
-                
+            if (where == 3){//down
+                if(this.isMove(this.x-1,this.y)){
+                    this.move(this.x-1,this.y);
+                    console.log("down");
+                    break;
+                }
             }
-        }
-        if (where == 4){//rigth
-            console.log("rigth");
-            if(this.isMove(this.x+1)[this.y]){
-                this.move(this.x+1,this.y);
-                
+            if (where == 4){//rigth
+                if(this.isMove(this.x+1,this.y)){
+                    this.move(this.x+1,this.y);
+                    console.log("rigth");
+                    break;
+                }
             }
         }
     }
 
 };
-
-
-function onKeyDown(event){
-    var wCode = 87; 
-	var aCode = 65;
-	var sCode = 83;
-	var dCode = 68;    
-    var keyCode = event.keyCode;
-
-	switch(keyCode){
-        case wCode:
-            console.log(mrP.isMove(9,9));
-            if (mrP.isMove(mrP.x+1, mrP.y)){             
-                   mrP.move();
-            }
-            break;
-        case aCode:
-            console.log(a);
-            break;    
-        case sCode:
-            console.log(s);
-            break;
-        case dCode:
-            console.log(d);
-            break;
-    }
-}
-
 
 window.onload = function(){
     init();
 };
-
-
-///canvas.addEventListener('keydown',onKeyDown,false);
-function randGo(){
-    var where = Math.floor(Math.random() * (5 - 1)) + 1;
-        if (where == 1){//up
-            console.log("up");
-            if(mrG.isMove(mrG.x)[mrG.y-1]){
-                mrG.move(mrG.x,mrG.y-1);
-                
-            }
-        }
-        if (where == 2){//left
-            console.log("left");
-            if(mrG.isMove(mrG.x-1)[mrG.y]){
-                mrG.move(mrG.x-1,mrG.y);
-                
-            }
-        }
-        if (where == 3){//down
-            console.log("down");
-            if(mrG.isMove(mrG.x-1)[mrG.y]){
-                mrG.move(mrG.x-1,mrG.y);
-                
-            }
-        }
-        if (where == 4){//rigth
-            console.log("rigth");
-            if(mrG.isMove(mrG.x+1)[mrG.y]){
-                mrG.move(mrG.x+1,mrG.y);
-                
-            }
-        }
-}
-
-
 
 document.onkeydown = function(event){
     var wCode = 87; 
@@ -241,21 +193,20 @@ document.onkeydown = function(event){
             if (mrP.isMove(mrP.x, mrP.y-1)){             
                    mrP.move(mrP.x,mrP.y-1);
             }
-            randGo();
+            mrG.randGo();
             break;
         case aCode:
             if(mrP.isMove(mrP.x-1,mrP.y)){
                 mrP.move(mrP.x-1,mrP.y);
             }
-            randGo();
-            
+            mrG.randGo();            
             break;    
         case sCode:
             if(mrP.isMove(mrP.x,mrP.y+1)){
                 mrP.move(mrP.x,mrP.y+1);
             }
             //mrG.move(mrG.x,mrG.y+1);
-            randGo();
+            mrG.randGo();
             break;
         case dCode:
             if(mrP.isMove(mrP.x+1,mrP.y)){
